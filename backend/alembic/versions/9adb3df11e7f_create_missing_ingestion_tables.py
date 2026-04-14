73190ff3b457082152528fbfef10ba9246002091
parv <parv@rhour.com>
2026-04-15 01:27:52 +0530

    pg fix 2

---
+++ backend/alembic/versions/9adb3df11e7f_create_missing_ingestion_tables.py
@@ -0,0 +1,24 @@
+"""create missing ingestion tables
+
+Revision ID: 9adb3df11e7f
+Revises: 6b328d59076e
+Create Date: 2026-04-14 19:55:06.873648
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9adb3df11e7f'
+down_revision = '6b328d59076e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
