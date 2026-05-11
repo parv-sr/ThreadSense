ff3f6d0f3dc7b2ce349ff77f6b72da465702d0b1
parv <parv@rhour.com>
2026-05-11 20:49:10 +0530

    Add DB migration for listing filter

---
+++ backend/alembic/versions/6e4d774441f4_add_floor_band_and_filters.py
@@ -0,0 +1,24 @@
+"""add_floor_band_and_filters
+
+Revision ID: 6e4d774441f4
+Revises: 64562d44a07f
+Create Date: 2026-05-11 15:18:00.880251
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6e4d774441f4'
+down_revision = '64562d44a07f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
